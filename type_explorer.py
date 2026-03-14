bc64fbfdf897936006f4f7f60629108cbc7a645c
Kevins Blūms <kevinsnikslv@gmail.com>
2026-03-14 03:17:14 +0200

    Izveidots veikals ar str, int, un float

---
+++ type_explorer.py
@@ -0,0 +1,10 @@
+Produkts1 = "Zemenes"
+skaits = int(3)
+cena_par_zemeni = float(0.80)
+Produkts2 = "Bumbieri"
+skaitsB = int(8)
+cena_par_bumbieri = float(1.2)
+print(f"Labdien, manā veikalā ir {Produkts1} un {Produkts2}")
+print(f"Prece: {Produkts1}")
+print(f"Daudzums: {skaits} gabali")
+print(f"Maksa: {skaits * cena_par_zemeni} EUR")
